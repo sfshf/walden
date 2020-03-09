3ae21b879b4cc437276d1a1e83768aeafb58c0d5
sfshf <sfshf@ubuntu.org>
2020-03-09 23:56:30 +0800

    Initializing the project.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sfshf/walden
+
+go 1.14
---
+++ local.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# # Initialize the Hugo project.
+#
+# ## 1. Initialize the site.
+# ## Creating:
+# ##   directories -- archetypes, content, data, layouts, static, themes
+# ##   files      -- archetypes/default.md, config.toml
+#
+# hugo new site . --force
+#
+# ## 2. Initialize the main module.
+# ## Creating:
+# ##   directories -- resources/_gen/assets, resources/_gen/images
+# ##   files       -- go.mod
+#
+# hugo mod init github.com/sfshf/walden
+
+
+# # Run locally.
+#
+# rm -rf public/
+#
+# hugo server -D
